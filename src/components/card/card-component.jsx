4e33014b4b39c2f65818ea7a88788f142034284c
Tehreem Arooj <mazharm0786@gmail.com>
2022-08-16 21:32:02 +0500

    fix: remove additonal \

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -9,7 +9,7 @@ class Card extends Component {
           src={`https://robohash.org/${id}?set=set2&size=180x180`}
           alt={`monster ${name}`}
         />
-        // <h2>{name}</h2>
+        <h2>{name}</h2>
         <p>{email}</p>
       </div>
     );

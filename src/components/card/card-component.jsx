31e062fb98f3d183a12de5fb63d46132dade0f5f
Tehreem Arooj <mazharm0786@gmail.com>
2022-08-16 21:33:10 +0500

    fix: remove extra slash from card-component

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -7,7 +7,7 @@ const Card = ({ monster }) => {
         src={`https://robohash.org/${id}?set=set2&size=180x180`}
         alt={`monster ${name}`}
       />
-      // <h2>{name}</h2>
+      <h2>{name}</h2>
       <p>{email}</p>
     </div>
   );
